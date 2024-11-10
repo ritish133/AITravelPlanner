c42574dca10162cd73b3ce6f30977e383fb4f532
Ritish Kumar Singh <ritish@Ritishs-MacBook-Air.local>
2024-11-10 21:30:33 +0530

    Updated UI

--- src/components/ui/custom/Hero.jsx
+++ src/components/ui/custom/Hero.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
-
+import "./Hero.css"
 import { Link } from 'react-router-dom'
 import { Button } from '../button'
 
 function Hero() {
   return (
-    <div className='flex flex-col items-center gap-9 w-screen h-screen lg:bg-[url("/bg.jpg")] bg-cover overflow-hidden'>
+    <div className='flex flex-col items-center gap-9 w-screen h-screen bg-[url("/bg.jpg")] bg-cover overflow-hidden'>
       
       <h1
       className='font-extrabold text-[50px] text-center mt-16'
       ><span className='text-[#f56551]'>Plan Your Next Trip with AI:</span> <br />
         Personalized Itineraries at Your Fingertips</h1>
 
-      <p className='text-xl text-gray-500 text-center'>Your personal trip planner and travel curator, creating custom itineraries tailored to your interests and budget.</p>
+      <p className='text-xl text-gray-500 text-center custom'>Your personal trip planner and travel curator, creating custom itineraries tailored to your interests and budget.</p>
    
       <Link to={'/create-trip'}>
         <Button> Get Started, It's Free </Button>
